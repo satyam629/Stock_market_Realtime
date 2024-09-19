46e3f200b25404b574e1cd30cde463f86a7539a5
Satyam <63797763+satyam629@users.noreply.github.com>
2024-09-19 10:17:45 +0530

    Update stockmarket.py

--- stockmarket.py
+++ stockmarket.py
@@ -1,17 +1,18 @@
 # Import libraries
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import sum, col, when, max, lag
-from snowflake.snowpark import Window
-from datetime import timedelta
+
 import altair as alt
 import streamlit as st
 import pandas as pd
 
 # Set page config
 st.set_page_config(layout="wide")
+from datetime import timedelta
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import sum, col, when, max, lag
+from snowflake.snowpark import Window
 
-# Get current session
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 @st.cache_data()
 def load_data():
